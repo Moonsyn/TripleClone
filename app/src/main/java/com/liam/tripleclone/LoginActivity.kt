7fc9458607b926058f41d23091ead3dd9a7cb7ff
Moonsyn <msy9427@naver.com>
2023-06-26 17:03:54 +0900

    Initial commit

---
+++ app/src/main/java/com/liam/tripleclone/LoginActivity.kt
@@ -0,0 +1,36 @@
+package com.liam.tripleclone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.liam.tripleclone.databinding.ActivityLoginBinding
+
+
+class LoginActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
+
+        init()
+    }
+
+    private fun init() {
+        binding.tvLookAround.setOnClickListener {
+            // 둘러보기 TODO
+        }
+        binding.btnNaver.setOnClickListener {
+            // 네이버 로그인 TODO
+        }
+
+        binding.btnLogin.setOnClickListener {
+            // 일반 로그인 TODO
+        }
+    }
+
+}
